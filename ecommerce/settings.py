0be64e89aa29405cdeaac1f0f9dd311af9f1c696
DaraJoe <deejayblinks@gmail.com>
2025-01-10 00:27:01 +0100

    Add SQLite database to sync local and deployed environments

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -110,14 +110,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Ensure this matches your database location
     }
 }
-
-
 
 
 
